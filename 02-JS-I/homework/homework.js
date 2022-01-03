70556f9e5c2e2184bf648d2c9793567469453820
Bryan de la cruz <bryan.delacruz.ramirez>
2022-01-03 16:17:49 -0500

    t working

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -166,13 +166,21 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if(numero === 0) {
+    return false
+  }
+  if (numero > 0) {
+    return "es positivo";
+  } else {  
+    return "es negativo"
+  }
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
-  // Tu código:
+  // Tu código:  
+  return (str + "!");
 }
 
 function combinarNombres(nombre, apellido) {
