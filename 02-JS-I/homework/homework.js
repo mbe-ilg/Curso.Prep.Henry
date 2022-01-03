a54650a0cfc8d394a99d73b18376513b9ab1ec72
Bryan de la cruz <bryan.delacruz.ramirez>
2022-01-03 16:52:26 -0500

    hw

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -195,7 +195,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var nombrecombinado  = ("hola" + nombre + "!");
+  var nombrecombinado  = ("hola " + nombre + "!");
   return nombrecombinado
 }
 
