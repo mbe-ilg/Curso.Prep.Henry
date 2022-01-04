ce524024918e2095fe25786f137d3ab2775dcaf6
Bryan de la cruz <bryan.delacruz.ramirez>
2022-01-04 12:14:55 -0500

    hwlol

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,7 +5,10 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  return Math.max (x , y);
+  if (x<y) {
+    return y
+  } 
+  return x;
 }
 
 function mayoriaDeEdad(edad) {
@@ -26,10 +29,10 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-  if (status == 1){
+  if (status === 1){
     return "Online";
   } 
-  if (status == 2){
+  if (status === 2){
     return "Away";
   }
     else {
@@ -44,7 +47,19 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!"
+  }
+  else if (idioma === "mandarin") {
+    return "Ni Hao!"
+  }  
+  else if (idioma === "ingles") {
+    return "Hello!"
+  }  
+  else {
+    return "Hola!"
 }
+
 
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
@@ -54,12 +69,32 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue" :
+      return "This is blue";
+    case "red" :
+      return "This is red";
+    case "green" :
+      return "This is green";
+    case "orange" :
+      return "This is orange";
+    default:  
+      return "Color no found"
+  }  
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch (numero) {
+    case numero = 10:
+      return "true";
+    case numero = 5:  
+      return "true";
+    default:
+      return "false"    
+  }
 }
 
 function estaEnRango(numero) {
