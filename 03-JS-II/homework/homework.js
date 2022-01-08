ad22bfe6949fd8aaaef840b0043caab01237425b
Bryan de la cruz <bryan.delacruz.ramirez>
2022-01-07 23:24:42 -0500

    ihappy

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -192,7 +192,11 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero > 99 && numero < 1000) {
+    console.log (numero);
+  } else  {
+    return false
+  }
 }
 
 function doWhile(numero) {
